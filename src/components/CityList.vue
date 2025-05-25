68a4f00c29909475e540f70ffab9d20ae387029c
walaaosamamoh <waloosama@gmail.com>
2025-05-26 02:56:14 +0300

    Get cities from local storage

---
+++ src/components/CityList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-for="city in savedCities" :key="city.id">
+    <CityCard :city="city" @click="goToCityView(city)" />
+  </div>
+
+  <p v-if="savedCities.length === 0">
+    No locations added. To start tracking a location, search in the field above.
+  </p>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import CityCard from "../components/CityCard.vue";
+
+const savedCities = ref([]);
+const apiKey = "5d96731c2640185932bfa687657372ff";
+
+const getCities = async () => {
+  if (localStorage.getItem("savedCities")) {
+    savedCities.value = JSON.parse(localStorage.getItem("savedCities"));
+
+    const requests = [];
+    savedCities.value.forEach((city) => {
+      requests.push(
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lon}&appid=${apiKey}&units=imperial`
+        )
+      );
+    });
+
+    const weatherData = await Promise.all(requests);
+
+    weatherData.forEach((value, index) => {
+      savedCities.value[index].weather = value.data;
+    });
+  }
+};
+await getCities();
+
+const router = useRouter();
+const goToCityView = (city) => {
+  router.push({
+    name: "cityView",
+    params: { state: city.state, city: city.city },
+    query: { lat: city.coords.lat, lon: city.coords.lon },
+  });
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,80 +1,100 @@
 <template>
-    <main class="container text-white">
-        <div class="pt-4 mb-8 relative">
-            <input type="text" v-model="searchQuery" 
-            @input="getSearchResults"
-            placeholder="Search for a city or state"
-            class="py-2 px-1 w-full bg-transparent border-b focus:outline-none 
-            focus:shadow-[0px_1px_0_0_#004E71] focus:border-weather-scondary" />
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:outline-none focus:shadow-[0px_1px_0_0_#004E71] focus:border-weather-scondary"
+      />
 
-            <ul v-if="openweathermapRsults" class="absolute bg-weather-scondary text-white w-full shadow-md
-           top-[66px]">
-                <p class=" py-2 px-1 " v-if="searchError">
-                    Sorry, something went wrong, please try again.
-                </p>
-                <p class=" py-2 px-1 " v-else-if="!searchError && searchQuery && openweathermapRsults.length === 0">
-                    No results match your query, try a different term.
-                </p>
-                <template v-else>
-                    <li v-for="(searchResult, index) in openweathermapRsults" 
-                    :key="index" @click="previewCity(searchResult)"
-                    class= "py-2 cursor-pointer px-1 " >
-                    {{ searchResult.name }},
-                    {{ searchResult.state || ''}},
-                    {{ searchResult.country }} 
-                    </li>
-                </template>
-            </ul>
-        </div>
-    </main>
+      <ul
+        v-if="openweathermapRsults"
+        class="absolute bg-weather-scondary text-white w-full shadow-md top-[66px]"
+      >
+        <p class="py-2 px-1" v-if="searchError">
+          Sorry, something went wrong, please try again.
+        </p>
+        <p
+          class="py-2 px-1"
+          v-else-if="
+            !searchError && searchQuery && openweathermapRsults.length === 0
+          "
+        >
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li
+            v-for="(searchResult, index) in openweathermapRsults"
+            :key="index"
+            @click="previewCity(searchResult)"
+            class="py-2 cursor-pointer px-1"
+          >
+            {{ searchResult.name }}, {{ searchResult.state || "" }},
+            {{ searchResult.country }}
+          </li>
+        </template>
+      </ul>
+    </div>
+    <div class="flex flex-col gap-4">
+      <Suspense>
+        <CityList />
+        <template #fallback>
+          <p>Loading...</p>
+        </template>
+      </Suspense>
+    </div>
+  </main>
 </template>
 
 <script setup>
-import {ref} from 'vue';
-import axios from 'axios';
-import { useRouter } from 'vue-router';
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import CityList from "../components/CityList.vue";
 
 const router = useRouter();
 
-const previewCity=(searchResult)=>{
-    console.log(searchResult);
-    const city = searchResult.name;
-    const state = searchResult.state;
-    router.push({
-        name: 'cityView',
-        params: {state: state, city: city},
-        query: {
-            lat: searchResult.lat,
-            lon: searchResult.lon,
-            preview: true
-        }
-    })
-}
+const previewCity = (searchResult) => {
+  console.log(searchResult);
+  const city = searchResult.name;
+  const state = searchResult.state;
+  router.push({
+    name: "cityView",
+    params: { state: state, city: city },
+    query: {
+      lat: searchResult.lat,
+      lon: searchResult.lon,
+      preview: true,
+    },
+  });
+};
 
 const searchQuery = ref("");
-const queryTimeout= ref(null);
+const queryTimeout = ref(null);
 const openweathermapRsults = ref([]);
-const searchError = ref(false)
-const apiKey= "5d96731c2640185932bfa687657372ff";
+const searchError = ref(false);
+const apiKey = "5d96731c2640185932bfa687657372ff";
 
-const getSearchResults = ()=>{
-    clearTimeout(queryTimeout.value);
-    queryTimeout.value=setTimeout(async () => {
-        if (searchQuery.value !== "") {
-            try{
-                const result = await axios.get(
-                `http://api.openweathermap.org/geo/1.0/direct?q=
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(
+          `http://api.openweathermap.org/geo/1.0/direct?q=
                 ${searchQuery.value}&limit=5&appid=${apiKey}`
-                );
-                openweathermapRsults.value= result.data;
-                console.log(openweathermapRsults.value);
-                console.log(openweathermapRsults.value.length);
-            }catch{
-                searchError.value = true
-            }
-            return;
-        }
-        openweathermapRsults.value = []
-    }, 300);
-}
+        );
+        openweathermapRsults.value = result.data;
+        console.log(openweathermapRsults.value);
+        console.log(openweathermapRsults.value.length);
+      } catch {
+        searchError.value = true;
+      }
+      return;
+    }
+    openweathermapRsults.value = [];
+  }, 300);
+};
 </script>
