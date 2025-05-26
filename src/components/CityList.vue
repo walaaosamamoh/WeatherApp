4171b09b00ad574ebb20d6df8d2d66c5de482230
walaaosamamoh <waloosama@gmail.com>
2025-05-26 03:38:37 +0300

    Add remove city feature

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -44,7 +44,7 @@ const goToCityView = (city) => {
   router.push({
     name: "cityView",
     params: { state: city.state, city: city.city },
-    query: { lat: city.coords.lat, lon: city.coords.lon },
+    query: { id: city.id, lat: city.coords.lat, lon: city.coords.lon },
   });
 };
 </script>
