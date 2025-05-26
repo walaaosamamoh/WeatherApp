fc87bb0cbaad6a3072d3048720ba623a1725c70a
walaaosamamoh <waloosama@gmail.com>
2025-05-26 07:24:53 +0300

    Correct the publicpath

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
